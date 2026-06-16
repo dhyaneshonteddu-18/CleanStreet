e77a6bde26f46ecf9f14d8c6b29fb154478d8e78
dhyaneshonteddu-18 <medhyanesh@gmail.com>
2026-06-16 22:16:55 +0530

    Added authentication guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 
+import { authGuard } from './auth.guard';
+
 import { Home } from './pages/home/home';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Profile } from './pages/profile/profile';
@@ -12,44 +14,15 @@ import { Register } from './pages/register/register';
 import { ComplaintDetails } from './pages/complaint-details/complaint-details';
 
 import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
+
 import { MyComplaints } from './pages/my-complaints/my-complaints';
 
 export const routes: Routes = [
 
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
-  },
-
-  {
-    path: 'home',
-    component: Home
-  },
-
-  {
-    path: 'dashboard',
-    component: Dashboard
-  },
-
-  {
-    path: 'profile',
-    component: Profile
-  },
-
-  {
-    path: 'report',
-    component: Report
-  },
-
-  {
-    path: 'complaints',
-    component: Complaints
-  },
-
-  {
-    path: 'admin-dashboard',
-    component: AdminDashboard
   },
 
   {
@@ -62,14 +35,52 @@ export const routes: Routes = [
     component: Register
   },
 
+  {
+    path: 'home',
+    component: Home,
+    canActivate: [authGuard]
+  },
 
-{
-  path: 'complaint-details',
-  component: ComplaintDetails
-},
-{
-  path:'my-complaints',
-  component: MyComplaints
-}
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'profile',
+    component: Profile,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'report',
+    component: Report,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'complaints',
+    component: Complaints,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'my-complaints',
+    component: MyComplaints,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'complaint-details',
+    component: ComplaintDetails,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'admin-dashboard',
+    component: AdminDashboard,
+    canActivate: [authGuard]
+  }
 
 ];
